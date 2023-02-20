5a3b43be180fba77eef11b3b141610eb2c080f67
Lucas Guiraldelli <lucas.guiraldelli@outlook.com>
2023-02-20 16:52:27 +0000

    functions

--- StarbucksProject/utils.py
+++ StarbucksProject/utils.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import missingno as msno
+
 def create_offer_id_col(val):
     if list(val.keys())[0] in ['offer id', 'offer_id']:
         return list(val.values())[0]
@@ -5,3 +8,9 @@ def create_offer_id_col(val):
 def create_amount_col(val):
     if list(val.keys())[0] in ['amount']:
         return list(val.values())[0]
+
+def first_infos(df: pd.DataFrame):
+    print(df.info())
+    msno.bar(df, figsize=(5,3), fontsize=15)
+    df.sample(3)
+
